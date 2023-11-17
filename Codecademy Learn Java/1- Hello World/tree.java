9b9e81f885a95ca645f1c8a520c45b554b06fb13
Mustafa Eftekin <m_eftekin@hotmail.com>
2023-11-17 18:15:05 +0300

    Create tree.java

---
+++ Codecademy Learn Java/1- Hello World/tree.java
@@ -0,0 +1,15 @@
+public class Tree {
+    public static void main(String[] args){
+      // Tree
+  
+      System.out.println("Hey there, I'm Mustafa and I'm learning to code in Java!");
+  
+      System.out.println("  *  ");
+      System.out.println(" *** ");
+      System.out.println(" *** ");
+      System.out.println("  *  ");
+      System.out.println("  *  ");   
+  
+  
+    }
+  }
